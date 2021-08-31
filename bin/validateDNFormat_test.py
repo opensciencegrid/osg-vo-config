b250b7acbb9225bbaff1aa42a20fb5e7c8a243b4
RyanEweSeng <ryantoh1001@gmail.com>
2021-08-31 13:49:43 -0500

    Edit test cases

--- bin/validateDNFormat_test.py
+++ bin/validateDNFormat_test.py
@@ -2,18 +2,14 @@
 
 import unittest
 from unittest.mock import patch, mock_open
-import validateDNFormat
+import validate_dn_format as validateDNFormat
 
 class TestValidate(unittest.TestCase):
 
     def setUp(self):
         validateDNFormat.errorLog.clear()
 
-    # def testGoodFormat(self, mock_os):
-    #     validateDNFormat.validate('.')
-    #     self.assertEqual(validateDNFormat.errorLog, [])
-
-    @patch('validateDNFormat.os.walk')
+    @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC=org/ST=Wisconsin/CN=bad-name.org\n' +
         '/DC=org/ST=Wisconsin/CN=Test Company\n'
@@ -21,15 +17,15 @@ class TestValidate(unittest.TestCase):
     def testNotMatchingSubjectCN(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
-            ('root\\folders', (), ('test.org.lsc',)),
+            ('root/folders', (), ('test.org.lsc',)),
         ]
         expectedError = [
-            'Error in \"root\\folders\\test.org.lsc\" at /DC=org/ST=Wisconsin/CN=bad-name.org',
+            'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin/CN=bad-name.org',
         ]
         validateDNFormat.validate('.')
         self.assertEqual(validateDNFormat.errorLog, expectedError)
     
-    @patch('validateDNFormat.os.walk')
+    @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC=org/ST=Wisconsin/CN=test.org\n' +
         '/DC=org/ST=Wisconsin\n'
@@ -37,16 +33,15 @@ class TestValidate(unittest.TestCase):
     def testLastField(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
-            ('root\\folders', (), ('test.org.lsc',)),
+            ('root/folders', (), ('test.org.lsc',)),
         ]
         expectedError = [
-            'Error in \"root\\folders\\test.org.lsc\" at /DC=org/ST=Wisconsin',
+            'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin',
         ]
         validateDNFormat.validate('.')
         self.assertEqual(validateDNFormat.errorLog, expectedError)
 
-
-    @patch('validateDNFormat.os.walk')
+    @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/C=/ST=Wisconsin/L=Madison/CN=test.org\n' +
         '/C=US/ST=Wisconsin/L=Madison/CN=Test Company\n'
@@ -54,16 +49,15 @@ class TestValidate(unittest.TestCase):
     def testBadKeyValuePair(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
-            ('root\\folders', (), ('test.org.lsc',)),
+            ('root/folders', (), ('test.org.lsc',)),
         ]
         expectedError = [
-            'Error in \"root\\folders\\test.org.lsc\" at /C=/ST=Wisconsin/L=Madison/CN=test.org',
+            'Error in \"root/folders/test.org.lsc\" at /C=/ST=Wisconsin/L=Madison/CN=test.org',
         ]
         validateDNFormat.validate('.')
         self.assertEqual(validateDNFormat.errorLog, expectedError)
-    
 
-    @patch('validateDNFormat.os.walk')
+    @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC/=org/ST=Wisconsin/CN=test.org\n' +
         '/DC=orgST=Wisconsin/CN=Test Company\n'
@@ -71,17 +65,14 @@ class TestValidate(unittest.TestCase):
     def testBadDelimiter(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
-            ('root\\folders', (), ('test.org.lsc',)),
+            ('root/folders', (), ('test.org.lsc',)),
         ]
         expectedError = [
-            'Error in \"root\\folders\\test.org.lsc\" at /DC/=org/ST=Wisconsin/CN=test.org',
-            'Error in \"root\\folders\\test.org.lsc\" at /DC=/org/ST=Wisconsin/CN=Test Company',
-            'Error in \"root\\folders\\test.org.lsc\" at DC=org/ST=Wisconsin/CN=test.org',
-            'Error in \"root\\folders\\test.org.lsc\" at /DC=orgST=Wisconsin/CN=Test Company',
+            'Error in \"root/folders/test.org.lsc\" at /DC/=org/ST=Wisconsin/CN=test.org',
+            'Error in \"root/folders/test.org.lsc\" at /DC=orgST=Wisconsin/CN=Test Company',
         ]
         validateDNFormat.validate('.')
         self.assertEqual(validateDNFormat.errorLog, expectedError)
-
 
 if __name__ == '__main__':
     unittest.main()
