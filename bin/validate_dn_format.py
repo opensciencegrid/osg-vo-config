ff0741670d424278b38664e5c10d7f3926223d44
RyanEweSeng <ryantoh1001@gmail.com>
2021-09-02 15:17:25 -0500

    Refactor validate function

--- bin/validate_dn_format.py
+++ bin/validate_dn_format.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import glob
 import os
 import re
 
@@ -9,27 +10,30 @@ regex_subject_cn = re.compile(r"(?<=CN=).*")
 error_log = []
 
 def validate(dir):
-    for root, subdirs, files in os.walk(dir):
-        for file in files:
-            if file.endswith('.lsc'):
-                file_path = os.path.join(root, file)
-                file_name = file[:-len('.lsc')]
-                with open(file_path, 'r') as f:  # reads .lsc files line by line
-                    lines = f.read().splitlines()
-                    if len(lines) != 2:
-                        error_log.append("Error in \"" + file_path + "\" .lsc file should contain exactly 2 DNs")
-                    subject_cn = regex_subject_cn.search(lines[0]).group()  # reads subject DN and gets the CN
-                    for line in lines:
-                        match = regex_dn.search(line)
-                        if not match:
-                            error_log.append("Error in \"" + file_path + "\" at " + line)
-                        else:
-                            if line == lines[0]:
-                                if file_name not in subject_cn:
-                                    error_log.append("Error in \"" + file_path + "\" at " + line)
-                                continue
-                            else:
-                                continue
+    for file_path in glob.glob(dir + "/*/*.lsc"):
+        with open(file_path, 'r') as f:  # reads .lsc files line by line
+            lines = f.read().splitlines()
+
+            if check_number_of_dn(lines):
+                error_log.append(f'Error in "{file_path}" .lsc file should contain exactly 2 DNs')
+            
+            subject_cn_value = regex_subject_cn.search(lines[0]).group()  # reads subject DN and gets the CN value
+            filename_without_ext = os.path.splitext(os.path.basename(file_path))[0]  # extract the file name without extension
+            if check_matching_cn(filename_without_ext, subject_cn_value):
+                error_log.append(f'Error in "{file_path}" at "{lines[0]}" subject CN value does not match the file name')
+
+            for line in lines:
+                if check_format(line):
+                    error_log.append(f'Error in "{file_path}" at "{line}" invalid format')
+
+def check_number_of_dn(lines):
+    return len(lines) != 2
+
+def check_matching_cn(filename_without_ext, cn_value):
+    return filename_without_ext not in cn_value
+
+def check_format(dn):
+    return not regex_dn.search(dn)
 
 def print_error_log():
     if error_log:
