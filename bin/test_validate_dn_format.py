04d6fb6dcae52dcd92e252b462d77cf1c76e31bf
RyanEweSeng <ryantoh1001@gmail.com>
2021-09-02 13:45:13 -0500

    Rename variables and functions to follow PEP 8

--- bin/test_validate_dn_format.py
+++ bin/test_validate_dn_format.py
@@ -2,77 +2,77 @@
 
 import unittest
 from unittest.mock import patch, mock_open
-import validate_dn_format as validateDNFormat
+import validate_dn_format
 
 class TestValidate(unittest.TestCase):
 
     def setUp(self):
-        validateDNFormat.errorLog.clear()
+        validate_dn_format.error_log.clear()
 
     @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC=org/ST=Wisconsin/CN=bad-name.org\n' +
         '/DC=org/ST=Wisconsin/CN=Test Company\n'
     ))
-    def testNotMatchingSubjectCN(self, mock_walk):
+    def test_not_matching_subject_cn(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
             ('root/folders', (), ('test.org.lsc',)),
         ]
-        expectedError = [
+        expected_error = [
             'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin/CN=bad-name.org',
         ]
-        validateDNFormat.validate('.')
-        self.assertEqual(validateDNFormat.errorLog, expectedError)
+        validate_dn_format.validate('.')
+        self.assertEqual(validate_dn_format.error_log, expected_error)
     
     @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC=org/ST=Wisconsin/CN=test.org\n' +
         '/DC=org/ST=Wisconsin\n'
     ))
-    def testLastField(self, mock_walk):
+    def test_last_field(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
             ('root/folders', (), ('test.org.lsc',)),
         ]
-        expectedError = [
+        expected_error = [
             'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin',
         ]
-        validateDNFormat.validate('.')
-        self.assertEqual(validateDNFormat.errorLog, expectedError)
+        validate_dn_format.validate('.')
+        self.assertEqual(validate_dn_format.error_log, expected_error)
 
     @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/C=/ST=Wisconsin/L=Madison/CN=test.org\n' +
         '/C=US/ST=Wisconsin/L=Madison/CN=Test Company\n'
     ))
-    def testBadKeyValuePair(self, mock_walk):
+    def test_bad_key_value_pair(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
             ('root/folders', (), ('test.org.lsc',)),
         ]
-        expectedError = [
+        expected_error = [
             'Error in \"root/folders/test.org.lsc\" at /C=/ST=Wisconsin/L=Madison/CN=test.org',
         ]
-        validateDNFormat.validate('.')
-        self.assertEqual(validateDNFormat.errorLog, expectedError)
+        validate_dn_format.validate('.')
+        self.assertEqual(validate_dn_format.error_log, expected_error)
 
     @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
         '/DC/=org/ST=Wisconsin/CN=test.org\n' +
         '/DC=orgST=Wisconsin/CN=Test Company\n'
     ))
-    def testBadDelimiter(self, mock_walk):
+    def test_bad_delimiter(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
             ('root/folders', (), ('test.org.lsc',)),
         ]
-        expectedError = [
+        expected_error = [
             'Error in \"root/folders/test.org.lsc\" at /DC/=org/ST=Wisconsin/CN=test.org',
             'Error in \"root/folders/test.org.lsc\" at /DC=orgST=Wisconsin/CN=Test Company',
         ]
-        validateDNFormat.validate('.')
-        self.assertEqual(validateDNFormat.errorLog, expectedError)
+        validate_dn_format.validate('.')
+        self.assertEqual(validate_dn_format.error_log, expected_error)
 
     @patch('validate_dn_format.os.walk')
     @patch('builtins.open', mock_open(read_data=
@@ -80,16 +80,16 @@ class TestValidate(unittest.TestCase):
         '/DC=org/ST=Wisconsin/CN=Test Company\n' +
         '/DC=org/ST=Wisconsin/CN=Third DN\n'
     ))
-    def testNumberOfDN(self, mock_walk):
+    def test_number_of_dn(self, mock_walk):
         mock_walk.return_value = [
             ('root', ('folders',), ()),
             ('root/folders', (), ('test.org.lsc',)),
         ]
-        expectedError = [
+        expected_error = [
             'Error in \"root/folders/test.org.lsc\" .lsc file should contain exactly 2 DNs'
         ]
-        validateDNFormat.validate('.')
-        self.assertEqual(validateDNFormat.errorLog, expectedError)
+        validate_dn_format.validate('.')
+        self.assertEqual(validate_dn_format.error_log, expected_error)
 
 if __name__ == '__main__':
     unittest.main()
--- bin/validate_dn_format.py
+++ bin/validate_dn_format.py
@@ -3,45 +3,45 @@
 import os
 import re
 
-regex = re.compile(r"^(/(?:DC|OU|C|ST|L|O)=[^/=]+)*(/CN=[^/=].+)$")
-regexCN = re.compile(r"(?<=CN=).*")
+regex_dn = re.compile(r"^(/(?:DC|OU|C|ST|L|O)=[^/=]+)*(/CN=[^/=].+)$")
+regex_subject_cn = re.compile(r"(?<=CN=).*")
 
-errorLog = []
+error_log = []
 
 def validate(dir):
     for root, subdirs, files in os.walk(dir):
         for file in files:
             if file.endswith('.lsc'):
-                filePath = os.path.join(root, file)
-                fileName = file[:-len('.lsc')]
-                with open(filePath, 'r') as f:  # reads .lsc files line by line
+                file_path = os.path.join(root, file)
+                file_name = file[:-len('.lsc')]
+                with open(file_path, 'r') as f:  # reads .lsc files line by line
                     lines = f.read().splitlines()
                     if len(lines) != 2:
-                        errorLog.append("Error in \"" + filePath + "\" .lsc file should contain exactly 2 DNs")
-                    subjectCN = regexCN.search(lines[0]).group()  # reads subject DN and gets the CN
+                        error_log.append("Error in \"" + file_path + "\" .lsc file should contain exactly 2 DNs")
+                    subject_cn = regex_subject_cn.search(lines[0]).group()  # reads subject DN and gets the CN
                     for line in lines:
-                        match = regex.search(line)
+                        match = regex_dn.search(line)
                         if not match:
-                            errorLog.append("Error in \"" + filePath + "\" at " + line)
+                            error_log.append("Error in \"" + file_path + "\" at " + line)
                         else:
                             if line == lines[0]:
-                                if fileName not in subjectCN:
-                                    errorLog.append("Error in \"" + filePath + "\" at " + line)
+                                if file_name not in subject_cn:
+                                    error_log.append("Error in \"" + file_path + "\" at " + line)
                                 continue
                             else:
                                 continue
 
-def printErrorLog():
-    if errorLog:
-        for error in errorLog:
+def print_error_log():
+    if error_log:
+        for error in error_log:
             print(error)
-        errorLog.clear()
+        error_log.clear()
 
 
 def main():
     dir = 'vomsdir'
     validate(dir)
-    printErrorLog()
+    print_error_log()
 
 if __name__ == '__main__':
     main()
