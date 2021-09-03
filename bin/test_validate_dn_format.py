3cd7dd26c6352ed19e61cddffad80e404f22d0e1
RyanEweSeng <ryantoh1001@gmail.com>
2021-09-03 12:24:02 -0500

    Update unit tests to be more robust

--- bin/test_validate_dn_format.py
+++ bin/test_validate_dn_format.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import unittest
-from unittest.mock import patch, mock_open
 import validate_dn_format
 
 class TestValidate(unittest.TestCase):
@@ -9,87 +8,37 @@ class TestValidate(unittest.TestCase):
     def setUp(self):
         validate_dn_format.error_log.clear()
 
-    @patch('validate_dn_format.os.walk')
-    @patch('builtins.open', mock_open(read_data=
-        '/DC=org/ST=Wisconsin/CN=bad-name.org\n' +
-        '/DC=org/ST=Wisconsin/CN=Test Company\n'
-    ))
-    def test_not_matching_subject_cn(self, mock_walk):
-        mock_walk.return_value = [
-            ('root', ('folders',), ()),
-            ('root/folders', (), ('test.org.lsc',)),
+    def test_number_of_dn(self):
+        dummy_lines = [
+            '/DC=org/ST=Wisconsin/CN=test.org',
+            '/DC=org/ST=Wisconsin/CN=Test Company',
+            '/DC=org/ST=Wisconsin/CN=Third DN',
         ]
-        expected_error = [
-            'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin/CN=bad-name.org',
+        self.assertTrue(validate_dn_format.check_number_of_dn(dummy_lines))
+
+    def test_not_matching_subject_cn(self):
+        dummy_lines = [
+            '/DC=org/ST=Wisconsin/CN=bad-name.org',
+            '/DC=org/ST=Wisconsin/CN=Test Company',
         ]
-        validate_dn_format.validate('.')
-        self.assertEqual(validate_dn_format.error_log, expected_error)
+        dummy_file_path = 'root/folders/test.org.lsc'
+        self.assertTrue(validate_dn_format.check_matching_cn(dummy_lines[0], dummy_file_path))
     
-    @patch('validate_dn_format.os.walk')
-    @patch('builtins.open', mock_open(read_data=
-        '/DC=org/ST=Wisconsin/CN=test.org\n' +
-        '/DC=org/ST=Wisconsin\n'
-    ))
-    def test_last_field(self, mock_walk):
-        mock_walk.return_value = [
-            ('root', ('folders',), ()),
-            ('root/folders', (), ('test.org.lsc',)),
-        ]
-        expected_error = [
-            'Error in \"root/folders/test.org.lsc\" at /DC=org/ST=Wisconsin',
-        ]
-        validate_dn_format.validate('.')
-        self.assertEqual(validate_dn_format.error_log, expected_error)
+    def test_no_cn(self):
+        dummy_line = '/DC=org/ST=Wisconsin'
+        self.assertTrue(validate_dn_format.check_format(dummy_line))
 
-    @patch('validate_dn_format.os.walk')
-    @patch('builtins.open', mock_open(read_data=
-        '/C=/ST=Wisconsin/L=Madison/CN=test.org\n' +
-        '/C=US/ST=Wisconsin/L=Madison/CN=Test Company\n'
-    ))
-    def test_bad_key_value_pair(self, mock_walk):
-        mock_walk.return_value = [
-            ('root', ('folders',), ()),
-            ('root/folders', (), ('test.org.lsc',)),
-        ]
-        expected_error = [
-            'Error in \"root/folders/test.org.lsc\" at /C=/ST=Wisconsin/L=Madison/CN=test.org',
-        ]
-        validate_dn_format.validate('.')
-        self.assertEqual(validate_dn_format.error_log, expected_error)
+    def test_bad_key_value_pair(self):
+        dummy_line = '/C=/ST=Wisconsin/L=Madison/CN=test.org'
+        self.assertTrue(validate_dn_format.check_format(dummy_line))
 
-    @patch('validate_dn_format.os.walk')
-    @patch('builtins.open', mock_open(read_data=
-        '/DC/=org/ST=Wisconsin/CN=test.org\n' +
-        '/DC=orgST=Wisconsin/CN=Test Company\n'
-    ))
-    def test_bad_delimiter(self, mock_walk):
-        mock_walk.return_value = [
-            ('root', ('folders',), ()),
-            ('root/folders', (), ('test.org.lsc',)),
-        ]
-        expected_error = [
-            'Error in \"root/folders/test.org.lsc\" at /DC/=org/ST=Wisconsin/CN=test.org',
-            'Error in \"root/folders/test.org.lsc\" at /DC=orgST=Wisconsin/CN=Test Company',
-        ]
-        validate_dn_format.validate('.')
-        self.assertEqual(validate_dn_format.error_log, expected_error)
-
-    @patch('validate_dn_format.os.walk')
-    @patch('builtins.open', mock_open(read_data=
-        '/DC=org/ST=Wisconsin/CN=test.org\n' +
-        '/DC=org/ST=Wisconsin/CN=Test Company\n' +
-        '/DC=org/ST=Wisconsin/CN=Third DN\n'
-    ))
-    def test_number_of_dn(self, mock_walk):
-        mock_walk.return_value = [
-            ('root', ('folders',), ()),
-            ('root/folders', (), ('test.org.lsc',)),
-        ]
-        expected_error = [
-            'Error in \"root/folders/test.org.lsc\" .lsc file should contain exactly 2 DNs'
-        ]
-        validate_dn_format.validate('.')
-        self.assertEqual(validate_dn_format.error_log, expected_error)
+    def test_first_delimiter(self):
+        dummy_line = 'C=US/ST=Wisconsin/L=Madison/CN=test.org'
+        self.assertTrue(validate_dn_format.check_format(dummy_line))
+    
+    def test_missing_delimiter(self):
+        dummy_line = '/C=US/ST=WisconsinL=Madison/CN=test.org'
+        self.assertTrue(validate_dn_format.check_format(dummy_line))
 
 if __name__ == '__main__':
     unittest.main()
